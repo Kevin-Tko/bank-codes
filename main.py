a57eba54667a261443831a0d6430c118636f95b1
Kevin-Tko <kelvnjogu@gmail.com>
2023-08-11 06:13:31 +0300

    simple app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/")
 def home_page():
-    return 'Welcome'
+    return render_template('home.html')
