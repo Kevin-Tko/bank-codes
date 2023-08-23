510ed6ee3ef1c6bc912b7a0c16df5668f2059211
Kevin-Tko <112008891+Kevin-Tko@users.noreply.github.com>
2023-08-23 11:32:09 +0000

    finalizations

--- data.py
+++ data.py
@@ -1,21 +1,16 @@
 import pandas as pd
 
 def bank_name(user_input):
-  df = pd.read_csv('HQ BICS.csv')
+  df = pd.read_csv('bics.csv')
   bank = df[df['Bank'].str.contains(user_input)].iloc[0, 0]
   return bank
 
 def bank_bic(user_input):
-  df = pd.read_csv('HQ BICS.csv')
+  df = pd.read_csv('bics.csv')
   swift = df[df['Bank'].str.contains(user_input)].iloc[0, 2]
   return swift
 
-def international_bank_name(user_input_iban):
-  df_int = pd.read_csv('iban.csv')
-  bank = df_int[df_int['BANKS'].str.contains(user_input_iban)].iloc[0, 0]
-  return bank
-  
-def internationa_bank_bic(user_input_iban):
-  df_int = pd.read_csv('iban.csv')
-  swift = df_int[df_int['BANKS'].str.contains(user_input_iban)].iloc[0, 2]
-  return swift
+def bank_city(user_input):
+  df = pd.read_csv('bics.csv')
+  city = df[df['Bank'].str.contains(user_input)].iloc[0, 1]
+  return city
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-import pandas as pd
-from data import bank_bic, bank_name, international_bank_name, internationa_bank_bic
+from data import bank_bic, bank_name, bank_city
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -8,23 +7,15 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home_page():
     return render_template('home.html')
 
-@app.route("/Kenyan BICS", methods=['GET','POST'])
+@app.route("/bics/", methods=['GET','POST'])
 def get_html_bic_input():
   if request.method == 'POST':
     try:
       user_input_bic = (request.form['bic']).upper()
-      return render_template('home.html',bank_name=bank_name(user_input_bic), bank_bic=bank_bic(user_input_bic))
+      name = user_input_bic
+      return render_template('result.html',bank_name=bank_name(name),bank_bic=bank_bic(name), bank_city=bank_city(name))
     except Exception as e:
-      return render_template('home.html', bank_name= 'bank not found', bank_bic= 'bic not found')
-
-@app.route("/International BICS", methods=['GET','POST'])
-def get_html_iban_input():
-  if request.method == 'POST':
-    try:
-      user_input_iban = (request.form['iban']).upper()
-      return render_template('home.html', int_bank_name= international_bank_name(user_input_iban), int_bank_bic=internationa_bank_bic(user_input_iban))
-    except Exception as e:
-      return render_template('home.html', int_bank_name= 'bank not found', int_bank_bic= 'bic not found')
+      return render_template('result.html', bank_name= 'bank not found', bank_bic= 'bic not found', bank_city='city not located')
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True, use_reloader=True)
